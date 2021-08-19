4d0f6c37923f314dff575f4f566bcce68feed2f0
pujie <pw.prayitno@gmail.com>
2021-08-19 07:22:42 +0700

    Update test2.js

--- test2.js
+++ test2.js
@@ -31,16 +31,18 @@ app.get('/getpicbyclientid/:id',(req,res,next)=>{
             new Promise((resolve,reject)=>x.map(row=>{
                 connection.doQuery(clientqueries.getPicByClientId({id:row.id}))
                 .then(pic=>{
-                    console.log('PIC',pic)
+                    //console.log('PIC',pic)
                     resolve (pic)
                 },picerr=>{
-                    console.log('Picerr',picerr)
+                    //console.log('Picerr',picerr)
                     reject (picerr)
                 })
             }))
             .then(x=>{
+                console.log('PIC res',x)
                 return x
             },y=>{
+                console.log('PIC err',y)
                 return y
             })
         )
