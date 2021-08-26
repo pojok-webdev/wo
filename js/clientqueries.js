0437d42b7c31eaebaa24d504fea068ba8e47a5ec
pujie <pw.prayitno@gmail.com>
2021-08-26 08:49:41 +0700

    Update clientqueries.js

--- js/clientqueries.js
+++ js/clientqueries.js
@@ -61,7 +61,7 @@ getMasterMaterial = _ => {
     return sql
 }
 getMasterDevice = _ => {
-    sql = ' select a.id,b.name,a.name,a.unit,a.brand,a.description from devices a '
+    sql = ' select a.id,b.name,a.name,a.unit,a.brand,a.description from pricelists2.devices a '
     sql+= 'left outer join devicecategories b on b.id=a.category_id '
     console.log(sql)
     return sql
